6ee3670047a5e477bb8afaea3bb8a1e3f37df516
mahdi rezainiya <mahdi.rezainiya@gmail.com>
2023-04-05 00:43:25 +0430

    change modified

--- websites design/style-5/index.js
+++ websites design/style-5/index.js
@@ -1,73 +1,74 @@
+const searchBox = document.querySelector('#search-books input');
+const ul = document.querySelector('ul')
+const link = document.querySelector('.button')
+const delate = `<span class="delete">حذف</span>`;
 const inputText = document.querySelector('#add-book input') ;
-const link = document.querySelector('.button') ;
-const ul = document.querySelector('ul') ;
-const delate = `<span class = "delete">حذف</span>`
-const checkbox = document.querySelector('#hide input') ;
-const inputSearch = document.querySelector('#search-books input') ;
+const checkbox = document.querySelector('#hide input') ; ;
 
-//
-
-link.addEventListener('click' , function(e){
-    const spanName = document.createElement('span') ;
-    spanName.className = 'name' ;
-    spanName.textContent = inputText.value ; 
-
-    const li = document.createElement('li') ;
-    li.appendChild(spanName) ;
-    li.innerHTML += delate ;
-    
-    ul.appendChild(li) ;
-
-    storeToLocalStorage(inputText.value) ;
-
-    inputText.value = "" ;
-    e.preventDefault() ;
-})
-
-let tasks ;
-if(localStorage.getItem('inputText') === null){
-    tasks = [] ;
-}
-else {
-    tasks = localStorage.getItem('inputText').split(',') ;
-}
-
-function storeToLocalStorage(task){
-    tasks.push(task) ;
-    localStorage.setItem('inputText' , tasks) ;
-}
-
-document.addEventListener('DOMContentLoaded' , function(e){
-    for(let item of tasks){
-        const spanName = document.createElement('span') ;
-        spanName.className = 'name' ;
-        spanName.textContent = item ;
-        
-        const li = document.createElement('li') ;
-        li.appendChild(spanName) ;
-        li.innerHTML += delate ;
-        ul.appendChild(li) ;
+searchBox.addEventListener('keyup' , function(e){
+    for(let book of ul.children){
+        if(book.firstElementChild.textContent.indexOf(searchBox.value) !== -1){
+            book.style.display = 'block' ;
+        }
+        else{
+            book.style.display = 'none' ; 
+        }
     }
 })
 
-////
+ul.addEventListener('click' , function(e){
+    if(e.target.className === 'delete'){
+        e.target.parentElement.remove();
+    }
+    removeFromLocalStorage(e.target.parentElement.children[0].textContent)
+})
 
 checkbox.addEventListener('change' , function(e){
     if(checkbox.checked){
         ul.style.display = 'none' ;
     }
     else{
-        ul.style.display = 'block' ;
+        ul.style.display = 'block'
     }
+    e.preventDefault()
 })
 
-////
+link.addEventListener('click' , function(e){
+    const spanName = document.createElement('span');
+    spanName.className = 'name' ;
+    spanName.textContent = inputText.value ;
+    const li = document.createElement('li');
+    li.appendChild(spanName);
+    li.innerHTML += delate ;
+    ul.appendChild(li);
+    storeToLocalStorage(inputText.value);
+    e.preventDefault()
+    inputText.value = "" ;
+})
 
-ul.addEventListener('click' , function(e){
-    if(e.target.className === 'delete' ){
-        e.target.parentElement.remove() ;
-        removeFromLocalStorage(e.target.parentElement.children[0].textContent) ;
-    }   
+var tasks ;
+if(localStorage.getItem('inputText') === null){
+    tasks = [] ;
+}
+else{
+    tasks = localStorage.getItem('inputText').split(',')
+}
+
+function storeToLocalStorage(task){
+    tasks.push(task);
+    localStorage.setItem('inputText' , tasks)
+}
+
+document.addEventListener('DOMContentLoaded' , function(e){
+    for(let item of tasks){
+        const spanName = document.createElement('span');
+        spanName.className = 'name' ;
+        spanName.textContent = item ;
+        const li = document.createElement('li');
+        li.appendChild(spanName);
+        li.innerHTML += delate ;
+        ul.appendChild(li)  
+    }
 })
 
 function removeFromLocalStorage(task){
@@ -76,26 +77,154 @@ function removeFromLocalStorage(task){
             tasks.splice(i , 1)
         }
     }
-
     if(tasks.length === 0){
-        localStorage.clear() ;
+        localStorage.clear();
     }
     else{
         localStorage.setItem('inputText' , tasks)
     }
 }
 
-////
 
-inputSearch.addEventListener('keyup' , function(e){
-    for(let book of ul.children){
-        if(book.firstElementChild.textContent.indexOf(inputSearch.value) !== -1 ){
-            book.style.display = 'block' ;
-        }
-        else{
-            book.style.display = 'none' ;
-        }
-    }
-})
 
-// localStorage.clear() ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
